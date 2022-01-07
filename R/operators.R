55adde9021dfc424f4b95dd3137d0161e8dd03c9
damianoluis <damiano.luis@gmail.com>
2022-01-07 15:14:10 -0600

    Add function composition operator

---
+++ R/operators.R
@@ -0,0 +1,11 @@
+#' Compose two functions
+#'
+#' @param ...Arguments to be passed to the left function
+#'
+#' @return A function calling f2(f1(...))
+#' @export
+#'
+#' @examples
+#' # Count missing data per column
+#' sapply(airquality, is.na %!>% sum)
+`%!>%` <- function(f1, f2) { function(...) { f2(f1(...)) } }
