5f096d5ba9e0a7d9ab443fbe092c881a1b966910
damianoluis <damiano.luis@gmail.com>
2022-02-10 14:53:24 -0600

    Modify operator symbol

--- R/ifelse.R
+++ R/ifelse.R
@@ -1,3 +1,20 @@
+#' Conditional Element Selection with Arbitrary Function
+#'
+#' @param test an object which can be coerced to logical mode.
+#' @param yes return values for true elements of test.
+#' @param no return values for false elements of test. Default is `yes`
+#' @param fun the function to be applied to the vector test.
+#'
+#' @return Same as ?ifelse
+#' @export
+#'
+#' @examples
+#' # Impute all missing values with 0
+#' iffun(is.na, airquality[, 1], 0)
+iffun <- function(fun, test, yes, no = test) {
+  ifelse(fun(test), yes, no)
+}
+
 #' @inherit iffun
 #' @examples
 #' f <- function() { NULL } # Function that returns NULL if error
@@ -14,21 +31,4 @@ ifnull <- function(test, yes, no = test) {
 #' ifna(airquality[, 1], 0)
 ifna   <- function(test, yes, no = test) {
   iffun(is.na, test, yes, no)
-}
-
-#' Conditional Element Selection with arbitrary function
-#'
-#' @param test an object which can be coerced to logical mode.
-#' @param yes return values for true elements of test.
-#' @param no return values for false elements of test. Default is `yes`
-#' @param fun the function to be applied to the vector test.
-#'
-#' @return Same as ?ifelse
-#' @export
-#'
-#' @examples
-#' # Impute all missing values with 0
-#' iffun(is.na, airquality[, 1], 0)
-iffun <- function(fun, test, yes, no = test) {
-  ifelse(fun(test), yes, no)
 }
